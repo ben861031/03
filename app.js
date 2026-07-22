f280adcc69db7c40a88e7acbbab0bb1b32e68a27
ben861031 <ben861031@gmail.com>
2026-07-22 14:57:23 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -990,6 +990,9 @@ const fileName =
 today.getFullYear() +
 String(today.getMonth()+1).padStart(2,'0') +
 String(today.getDate()).padStart(2,'0') +
+'_' +
+String(today.getHours()).padStart(2,'0') +
+String(today.getMinutes()).padStart(2,'0') +
 '.json';
 
 a.href = URL.createObjectURL(blob);
