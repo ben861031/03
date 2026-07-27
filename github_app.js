549af8d4e7241ddc2178170e3d77925e21b8c9ac
ben861031 <ben861031@gmail.com>
2026-07-27 08:24:08 +0800

    Add files via upload

--- github_app.js
+++ github_app.js
@@ -1,12 +1,15 @@
-﻿const AppTemplate = `
+const AppTemplate = `
 <div class="sidebar">
 <div class="logo collapsed-logo">
 ☰
 </div>
 <div class="logo expanded-logo">
 <svg class="logo-mark" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">
-<rect x="3" y="5" width="18" height="14" rx="2"></rect>
-<path d="m3 7 9 6 9-6"></path>
+  <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
+  <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
+  <path d="M10 9H8"></path>
+  <path d="M16 13H8"></path>
+  <path d="M16 17H8"></path>
 </svg>
 <span>發文時程管理平台</span>
 </div>
